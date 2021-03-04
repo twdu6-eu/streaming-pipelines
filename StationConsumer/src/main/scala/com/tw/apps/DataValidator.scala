1c2bcf9e8e3667468ea30035b6749387883a8227
Pablo <pablo.porto@thoughtworks.com>
2021-03-04 17:45:28 +0100

    Story #20: Add DataValidator Spark app
    Co-authored-by: Pablo Kerestey <pavlo.kerestey@thoughtworks.com>

--- StationConsumer/src/main/scala/com/tw/apps/DataValidator.scala
+++ StationConsumer/src/main/scala/com/tw/apps/DataValidator.scala
@@ -1,11 +1,40 @@
 package com.tw.apps
 
-import com.amazon.deequ.VerificationSuite
-import com.amazon.deequ.checks.{Check, CheckLevel}
-import org.apache.spark.sql.{DataFrame, Dataset}
+import com.amazon.deequ.{VerificationResult, VerificationSuite}
+import com.amazon.deequ.checks.{Check, CheckLevel, CheckStatus}
+import com.amazon.deequ.constraints.ConstraintStatus
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 
 object DataValidator {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder
+      .appName("StationConsumerDataValidator")
+      .getOrCreate()
+
+    val stationDataDF = spark.read.format("csv").load(dockeargs(0))
+    val verificationResult = validate(stationDataDF)
+
+    printReport(verificationResult)
+  }
+
+  private def printReport(verificationResult: VerificationResult) = {
+    if (verificationResult.status == CheckStatus.Success) {
+      println("The data passed the test, everything is fine!")
+    } else {
+      println("We found errors in the data:\n")
+
+      val resultsForAllConstraints = verificationResult.checkResults
+        .flatMap { case (_, checkResult) => checkResult.constraintResults }
+
+      resultsForAllConstraints
+        .filter {
+          _.status != ConstraintStatus.Success
+        }
+        .foreach { result => println(s"${result.constraint}: ${result.message.get}") }
+    }
+  }
 
   def validate(dataDF: DataFrame) = {
     VerificationSuite()
